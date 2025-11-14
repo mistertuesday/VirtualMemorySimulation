4215743167f1386d052ba42ce2d7a2d05bdbda7d
Mister Tuesday <mistertuesday@protonmail.com>
2025-11-14 10:42:11 -0600

    created tuple object

---
+++ src/tuple.java
@@ -0,0 +1,15 @@
+
+
+public class tuple {
+    int x1;
+    int x2;
+
+    public tuple(int x1, int x2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+
+    public int getX() return x1;
+    public int getY() return x2;
+}
